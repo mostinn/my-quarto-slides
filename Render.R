540c7e12a4a769da934bc0cd9fbc61d0fa3944b0
mostinn <mostinn@gmail.com>
2025-06-13 13:39:25 -0400

    Initial  commit with Qarto slides

--- Render.R
+++ Render.R
@@ -1,5 +1,16 @@
 
 # -Render ----------------------------------------------------------------------------------------------
+
+# For slides
+quarto::quarto_render("Synthetic-biology.qmd", output_format = "revealjs")
+
+# For PDF with custom dimensions
+quarto::quarto_render("Synthetic-biology.qmd", output_format = "pdf")
+
+system('quarto render Synthetic-biology.qmd --to pdf -M papersize:custom -M geometry:"paperwidth=16in,paperheight=9in,margin=0.5in"')
+
+ # - ----
+
 quarto::quarto_render("Synthetic-biology.qmd", output_format = "pdf")
 quarto::quarto_render("Synthetic-biology.qmd", output_format = "pptx")
 quarto::quarto_render("Synthetic-biology.qmd", output_format = "docx")
