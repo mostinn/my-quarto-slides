0210ac28a80cb23a08fdefdb28ffad8da44ed8cb
mostinn <mostinn@gmail.com>
2025-06-13 10:33:56 -0400

    Fixing slide formatting

--- Git-Brandeis.bash
+++ Git-Brandeis.bash
@@ -6,5 +6,8 @@ git commit -m "Add HTML of SynBio"
 git remote add origin https://github.com/mostinn/brandeis.git
 git push -u origin master
 
-
+#_ Just push your changes:
+git add .
+git commit -m "Add slides"
+git push
 
