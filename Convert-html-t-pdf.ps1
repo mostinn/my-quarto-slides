6983462f311a69b53b7a847e519982082ba8c49a
mostinn <mostinn@gmail.com>
2025-06-14 15:30:11 -0400

    Add file folder

--- Convert-html-t-pdf.ps1
+++ Convert-html-t-pdf.ps1
@@ -1,13 +1,3 @@
-﻿#quarto render Synthetic-biology.qmd --to revealjs
-decktape reveal Synthetic-biology.html Synthetic-biology.pdf --size 1600x900
-
-quarto publish quarto-pub
-
-quarto publish https://github.com/mostinn/brandeis Synthetic-biology.qmd
-
-quarto publish https://github.com/mostinn/brandeis Synthetic-biology.qmd --server github --account mostinn --repository brandeis
-
-quarto publish gh-pages Synthetic-biology.qmd --server https://github.com/mostinn/brandeis
-
-quarto publish gh-pages Synthetic-biology.qmd
+﻿#_Convert html slides to pdf -remember to render the file (Shief-Ctrl-K)
+decktape reveal ./index.html Synthetic-biology.pdf --size 1600x900
 
--- Git-Brandeis.bash
+++ Git-Brandeis.bash
@@ -1,13 +1,8 @@
-## Start a git session in the directory (and get the right directory for the git tab):
-
-#git init
-git add Synthetic-biology.html
-git commit -m "Add HTML of SynBio"
-git remote add origin https://github.com/mostinn/brandeis.git
-git push -u origin master
-
-#_ Just push your changes:
+git init
 git add .
-git commit -m "Add slides"
-git push
+git commit -m "Add Quarto slides"
+git branch -M main
+git remote add origin https://github.com/mostinn/my-quarto-slides.git
+git push -u origin main
+
 
